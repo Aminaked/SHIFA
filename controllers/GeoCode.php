c2f836d615049870ef1251c82696c037f7ff6785
amina <aminake005@gmail.com>
2025-01-29 00:09:13 +0000

    geocoding addresses

--- controllers/GeoCode.php
+++ controllers/GeoCode.php
@@ -1,23 +1,31 @@
 <?php
 function geocodeAddress($address) {
-    $apiKey = '67979ed357e9b903835538psg98272b';  
+    $apiKey = '67979ed357e9b903835538psg98272b';
+    $encodedAddress   = urlencode($address); 
+    $url = "https://geocode.maps.co/search?q=" . $encodedAddress . "&api_key=" . $apiKey;
+  
 
-    $encodedAddress = urlencode($address); 
-   // $url = "https://api.mapmaker.com/geocode?address=" . $encodedAddress . "&key=" . $apiKey;
-    $url = "https://geocode.maps.co/search?q=". $encodedAddress . "&key=" . $apiKey;
-     https://geocode.maps.co/search?q=Pharmacie+de.+Chawal,+%D8%A7%D9%84%D8%B7%D8%B1%D9%8A%D9%82+%D8%A7%D9%84%D9%88%D9%8F%D8%B7%D9%86%D9%8A+%D8%B1%D9%82%D9%85+4%D8%8C+Boukadir&key=67979ed357e9b903835538psg98272b&limit=1
 
-    $response = file_get_contents($url);
-    var_dump($response);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); // Get HTTP status code
+    curl_close($ch);
+
+    // Debug: Log the raw response and HTTP status code
+    file_put_contents('geocode_debug.log', "URL: $url\nHTTP Code: $httpCode\nResponse: $response\n\n", FILE_APPEND);
+
     $data = json_decode($response, true);
 
-    if (isset($data['results'][0])) {
-        $location = $data['results'][0]['geometry']['location'];
+    // Check if the response contains valid data
+    if (!empty($data) && isset($data[0]['lat']) && isset($data[0]['lon'])) {
         return [
-            'latitude' => $location['lat'],
-            'longitude' => $location['lng']
+            'latitude' => $data[0]['lat'],
+            'longitude' => $data[0]['lon']
         ];
     }
 
-    return null;  
+    return null; // Geocoding failed
 }
