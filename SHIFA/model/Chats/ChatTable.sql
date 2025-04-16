d89712c390b3dc04f0a0ffd5af721fa7e506a4f8
Amina Ked <aminake005@gmail.com>
2025-04-16 18:30:55 +0200

    changed the chat db table structure

--- SHIFA/model/Chats/ChatTable.sql
+++ SHIFA/model/Chats/ChatTable.sql
@@ -1,18 +1,14 @@
 CREATE TABLE chats (
     chat_id INT AUTO_INCREMENT PRIMARY KEY,
-    sender_id INT NOT NULL,                         
-    receiver_id INT NOT NULL,                        
-    message TEXT NOT NULL,                           
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,    
-    is_read BOOLEAN DEFAULT FALSE,     
-    conversation_id INT NOT NULL,               
-    FOREIGN KEY (sender_id) 
-        REFERENCES clients(client_id) 
-        ON DELETE CASCADE,                           
-    FOREIGN KEY (receiver_id) 
-        REFERENCES pharmacy(pharmacy_id) 
-        ON DELETE CASCADE,     
-         FOREIGN KEY (conversation_id) 
+    sender_id INT NOT NULL,
+    sender_type ENUM('client', 'pharmacy') NOT NULL,
+    receiver_id INT NOT NULL,
+    receiver_type ENUM('client', 'pharmacy') NOT NULL,
+    message TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    is_read BOOLEAN DEFAULT FALSE,
+    conversation_id INT NOT NULL,
+    FOREIGN KEY (conversation_id) 
         REFERENCES conversations(conversation_id) 
-        ON DELETE CASCADE                     
+        ON DELETE CASCADE
 );
